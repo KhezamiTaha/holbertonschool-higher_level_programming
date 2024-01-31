b7dd78288d24c8bc235a285eb6533c5764bdd28d
KhezamiTaha <khezamitaha10@gmail.com>
2024-01-31 15:25:51 +0000

    If/else, loops, functions

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    upper = ""
+    for i in range(len(str)):
+        if (ord(str[i]) >= ord('a') and ord(str[i]) <= ord('z')):
+            upper = upper + chr(ord(str[i]) - 32)
+        else:
+            upper = upper + str[i]
+    print(upper)
