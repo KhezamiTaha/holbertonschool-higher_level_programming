797f4bf7b86fbb4eda203127e570d92592e01e94
KhezamiTaha <khezamitaha10@gmail.com>
2024-02-12 14:59:29 +0000

    Python - Data Structures: Lists, Tuples

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    return list(map(lambda x: [x[0] ** 2, x[1] ** 2, x[2] ** 2], matrix))
+    new_ma = list(map(lambda row: list(map(lambda x: x ** 2, row)), matrix))
+    return new_ma
