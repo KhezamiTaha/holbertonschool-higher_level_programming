7cf6de719c2e43c2538b9e3838c27ebda1498418
KhezamiTaha <khezamitaha10@gmail.com>
2024-02-12 15:17:43 +0000

    Python - Data Structures: Lists, Tuples

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-def square_matrix_simple(matrix=[]):
-    return list(map(lambda row: list(map(lambda x: x ** 2, row)), matrix))
-
 def search_replace(my_list, search, replace):
     return list(map(lambda x: replace if x == search else x, my_list))
