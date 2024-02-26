e497143d244e3ce9a75c39b448f9ef1de115a8d0
KhezamiTaha <khezamitaha10@gmail.com>
2024-02-26 17:02:55 +0000

    python-more_classes

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+Rectangle height and weight
+"""
+
+
+class Rectangle:
+    """
+    A class representing a rectangle.
+
+    This class provides a blueprint for creating rectangle objects.
+    """
+
+    def __init__(self, width=0, height=0):
+        """
+        initiantion
+        """
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value <= 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value <= 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
