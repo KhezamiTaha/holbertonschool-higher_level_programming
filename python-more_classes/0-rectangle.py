359bda559a91df7f1493aff6f3ce79cb9ee99cbc
KhezamiTaha <khezamitaha10@gmail.com>
2024-02-26 11:52:55 +0000

    python-more_classes

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This module defines a class called Rectangle.
+"""
+
+
+class Rectangle:
+    """
+    A class representing a rectangle.
+
+    This class provides a blueprint for creating rectangle objects.
+    """
+
+    pass
