b1281f225164f4d600389dcbbe7ec8730791cb66
KhezamiTaha <khezamitaha10@gmail.com>
2024-02-27 17:37:52 +0000

    python-more_classes

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -92,9 +92,8 @@ class Rectangle:
         """
         if self.__height * self.__width == 0:
             return ""
-        return ((("#" * self.__width) + "\n") * (self.__height - 1)) + (
-            "#" * self.__width
-        )
+        return (((str(self.print_symbol) * self.__width) + "\n") * (
+            self.__height - 1)) + (str(self.print_symbol) * self.__width)
 
     def __repr__(self):
         return "Rectangle(" + str(self.__width) + ", " + str(
