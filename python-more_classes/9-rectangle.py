9332fd6c2429ce5f68d15da040b0dd100fb4cf70
KhezamiTaha <khezamitaha10@gmail.com>
2024-02-27 21:31:27 +0000

    python-more_classes

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -19,7 +19,7 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
-        Rectangle.number_of_instances += 1
+        type(self).number_of_instances += 1
 
     """
     int: The width of the rectangle.
