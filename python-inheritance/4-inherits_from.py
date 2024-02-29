569ddb42704f5bd7a7d1daae6d787bb736bed99f
KhezamiTaha <khezamitaha10@gmail.com>
2024-02-29 21:59:00 +0000

    python-everything_is_object

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python3
 """
-Docstring
+Module: is_same_class
+Description: Provides a function to check if an object
 """
 
 
 def inherits_from(obj, a_class):
     """
-    Docstring
+    Check if an object is an instance of a specific class.
+
+    Args:
+        obj: The object to check.
+        a_class: The class to check against.
+
+    Returns:
+        bool: True if the object is an instance
     """
     return issubclass(type(obj), a_class) and type(obj) != a_class
