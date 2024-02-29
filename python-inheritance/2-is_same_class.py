6cffdd0fd22473ee32f37bf4c4e9630b9d1f4acb
KhezamiTaha <khezamitaha10@gmail.com>
2024-02-29 21:40:50 +0000

    python-everything_is_object

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Module: is_same_class
+Description: Provides a function to check if an object
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    Check if an object is an instance of a specific class.
+
+    Args:
+        obj: The object to check.
+        a_class: The class to check against.
+
+    Returns:
+        bool: True if the object is an instance
+    """
+    return isinstance(obj, a_class)
