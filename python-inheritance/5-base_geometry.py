bf157fb88a93280c8c923b3f1dfd388cecb6e8f8
KhezamiTaha <khezamitaha10@gmail.com>
2024-02-29 22:05:46 +0000

    python-everything_is_object

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Module: BaseGeometry
+Description: Defines a base class for geometric shapes.
+"""
+
+
+class BaseGeometry:
+    """
+    A base class for geometric shapes.
+    """
+
+    pass
