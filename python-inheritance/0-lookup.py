9c68eb617fd4bffde814e21782673bdadd06520a
KhezamiTaha <khezamitaha10@gmail.com>
2024-02-29 20:51:13 +0000

    python-everything_is_object

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
 """
-Module: mylookup
-Description: Defines a function to inspect attributes and methods of an object.
+Provides a function to inspect the attributes and methods of an object.
 """
+
 
 def lookup(obj):
     """
-    Returns the list of attributes and methods of an object.
+    Return the list of attributes and methods of an object.
 
     Args:
         obj: The object to inspect.
 
     Returns:
-        list: A list of strings containing the names of the attributes and methods
+        list: A list of strings containing the names
         of the given object.
     """
     return dir(obj)
