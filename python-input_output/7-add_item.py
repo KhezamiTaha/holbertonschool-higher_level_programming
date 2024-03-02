897b897698d7da69d9226155093b8aef1cdf7415
KhezamiTaha <khezamitaha10@gmail.com>
2024-03-02 21:26:59 +0000

    def load_from_json_file(filename):

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,16 +1,48 @@
 #!/usr/bin/python3
+"""
+Script: add_to_json_file
+Description: Appends command-line arguments to a JSON file.
+"""
+
+
 import sys
 from os.path import exists
-
-
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-listy = []
-for arg in sys.argv[1: ]:
-    listy.append(arg)
-if exists("add_item.json"):
-    list2= load_from_json_file("add_item.json")
-    list_final = list2 + listy
-else:
-    list_final = listy
-save_to_json_file(list_final, "add_item.json")
+
+
+def main():
+    """
+    Append command-line arguments to a JSON file.
+
+    If the JSON file exists, it loads its content into a list,
+    then appends the command-line arguments to the list.
+    Finally, it saves the combined list back to the JSON file.
+
+    If the JSON file does not exist, it creates a new list
+    containing only the command-line arguments and saves it
+    to the JSON file.
+
+    Args:
+        None
+
+    Returns:
+        None
+    """
+    listy = sys.argv[1:]
+    filename = "add_item.json"
+
+    if exists(filename):
+        try:
+            list2 = load_from_json_file(filename)
+            list_final = list2 + listy
+        except Exception as e:
+            print(f"Error loading data from '{filename}': {e}")
+            list_final = listy
+    else:
+        list_final = listy
+
+    save_to_json_file(list_final, filename)
+
+if __name__ == "__main__":
+    main()
