12f2faf4ffad0e426393e8d080adf87294394fe0
KhezamiTaha <khezamitaha10@gmail.com>
2024-03-04 09:18:27 +0000

    first class Base

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Module: base
+Desc: Defines Base class with identifier.
+"""
+
+
+class Base:
+    """
+    Base class for objects with an identifier.
+
+    Attrs:
+        __nb_objects (int): Number of objects created.
+
+    Methods:
+        __init__(self, id=None): Initializes object with optional id.
+    """
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """
+        Initializes a Base object.
+
+        Args:
+            id (int): Optional identifier. If not provided,
+                      assigns incremented object count.
+        """
+        if id is not None:
+            self.id = id
+        else:
+            type(self).__nb_objects += 1
+            self.id = type(self).__nb_objects
