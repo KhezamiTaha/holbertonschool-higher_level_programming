420a7ee2438ea149411f761d278fbfecd45f85b1
KhezamiTaha <khezamitaha10@gmail.com>
2024-03-04 11:26:17 +0000

    update(self, *args, **kwargs)

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -109,15 +109,12 @@ class Rectangle(Base):
                 str(self.__x) + "/" + str(self.__y) + " - " +
                 str(self.__width) + "/" + str(self.__height))
 
-    def update(self, *args):
-        """update attributes using *args"""
-        if len(args) >= 1:
-            self.id = args[0]
-        if len(args) >= 2:
-            self.width = args[1]
-        if len(args) >= 3:
-            self.height = args[2]
-        if len(args) >= 4:
-            self.x = args[3]
-        if len(args) >= 5:
-            self.y = args[4]
+    def update(self, *args, **kwargs):
+        """update for the print function"""
+        if args:
+            attrs = ["id", "width", "height", "x", "y"]
+            for i, arg in enumerate(args):
+                setattr(self, attrs[i], arg)
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
