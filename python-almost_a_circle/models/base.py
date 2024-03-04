001dbe3199598850540de1ee2a7b45f8ef6bdd89
KhezamiTaha <khezamitaha10@gmail.com>
2024-03-04 18:06:14 +0000

    class Square

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -5,6 +5,7 @@ Desc: Defines Base class with identifier.
 """
 import json
 
+
 class Base:
     """
     Base class for objects with an identifier.
@@ -31,9 +32,18 @@ class Base:
         else:
             type(self).__nb_objects += 1
             self.id = type(self).__nb_objects
-    
+
     @staticmethod
     def to_json_string(list_dictionaries):
-        if  not list_dictionaries or list_dictionaries is []:
+        """
+        Returns the JSON string representation of list_dictionaries.
+
+        Args:
+            list_dictionaries (list): List of dictionaries.
+
+        Returns:
+            str: JSON string representation of list_dictionaries.
+        """
+        if not list_dictionaries:
             return "[]"
         return json.dumps(list_dictionaries)
