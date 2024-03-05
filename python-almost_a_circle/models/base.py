e68181cd0314738f624ee315d61c5b376f40be17
KhezamiTaha <khezamitaha10@gmail.com>
2024-03-05 08:30:10 +0000

    class Square

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -41,3 +41,23 @@ class Base:
         if list_dictionaries is None:
             return "[]"
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """
+        Saves a list of instances to a JSON file.
+
+        Args:
+            list_objs (list): A list of instances.
+
+        Notes:
+            - If list_objs is None, an empty list will be saved.
+            - The filename is <Class name>.json.
+            - The file will be overwritten if it already exists.
+        """
+        filename = cls.__name__ + ".json"
+        if list_objs is None:
+            list_objs = []
+        dictonary_list = [obj.to_dictionary() for obj in list_objs]
+        with open(filename, 'w') as file:
+            file.write(cls.to_json_string(dictonary_list))
