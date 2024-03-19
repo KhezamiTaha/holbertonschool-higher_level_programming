186ff779fa65d9793383b913d1e94144f9ebaadf
KhezamiTaha <khezamitaha10@gmail.com>
2024-03-19 11:37:24 +0000

    TABLE DESCRIPTION

---
+++ SQL_introduction/5-full_table.sql
@@ -0,0 +1,5 @@
+-- 5-full_table.sql
+SELECT COLUMN_NAME, DATA_TYPE, CHARACTER_MAXIMUM_LENGTH, IS_NULLABLE
+FROM information_schema.COLUMNS
+WHERE TABLE_SCHEMA = 'hbtn_0c_0'
+AND TABLE_NAME = 'first_table';
