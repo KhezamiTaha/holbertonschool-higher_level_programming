28112d93e2f623cc2bbc20ec5c0c1a3f7db82de0
KhezamiTaha <khezamitaha10@gmail.com>
2024-03-22 14:43:46 +0000

    JOIN and subqueries

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -3,8 +3,5 @@ SELECT tv_genres.name
 FROM tv_shows
     JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
     JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-WHERE tv_shows.id = (
-    SELECT tv_shows.id
-    FROM tv_shows
-    WHERE title = "Dexter")
+WHERE title = "Dexter"
 ORDER BY name;
