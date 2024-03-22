67e70072e22efb8ea523296a4c379f41a1b1504e
KhezamiTaha <khezamitaha10@gmail.com>
2024-03-22 12:07:24 +0000

    JOIN and GROUP BY

---
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,7 @@
+-- 13-count_shows_by_genre.sql
+SELECT tv_genres.name AS "genre", COUNT(tv_shows.title) AS "number_of_shows"
+FROM tv_shows
+    JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+    JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+GROUP BY tv_genres.name
+ORDER BY number_of_shows DESC;
