f495feb0fa43393ca3c8705ead8a3115cd23ca35
KhezamiTaha <khezamitaha10@gmail.com>
2024-03-25 18:04:35 +0000

    WHERE name = %s

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states ORDER BY states.id")
+    cursor.execute("SELECT * FROM states WHERE name = %s ORDER BY states.id", (sys.argv[4],))
     rows = cursor.fetchall()
     for row in rows:
         print(row)
