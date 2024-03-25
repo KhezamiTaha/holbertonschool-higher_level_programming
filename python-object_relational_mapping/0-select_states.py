8330aa28b8f97e5baf46efa494bd72fa4e9bf525
KhezamiTaha <khezamitaha10@gmail.com>
2024-03-25 15:23:02 +0000

    trying MYSQLdb

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,8 +1,24 @@
 #!/usr/bin/python3
+'''
+DOcstring MySQLdb
+'''
+
+
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
+     """
+    Connects to a MySQL database and retrieves data from the states table.
+
+    Usage:
+        $ python3 script.py <username> <password> <database>
+
+    Arguments:
+        username (str): MySQL database username.
+        password (str): MySQL database password.
+        database (str): Name of the MySQL database to connect to.
+    """
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states ORDER BY states.id")
