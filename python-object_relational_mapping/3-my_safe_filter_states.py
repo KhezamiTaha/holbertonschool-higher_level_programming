52234dbf22bea1972c40d7948c8c06f7fe97f3c6
KhezamiTaha <khezamitaha10@gmail.com>
2024-03-26 14:54:27 +0000

    cursor.execute(query, (sys.argv[4],))

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+'''
+DOcstring MySQLdb
+'''
+
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    """
+    Connects to a MySQL database and retrieves data from the states table.
+    """
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+    cursor = db.cursor()
+    query = "SELECT * FROM states WHERE name = %s ORDER BY states.id"
+
+    cursor.execute(query, (sys.argv[4],))
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    db.close()
