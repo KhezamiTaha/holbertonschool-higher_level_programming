3d03806aaba90758cdfc19544ddcc1c78189f16e
KhezamiTaha <khezamitaha10@gmail.com>
2024-03-26 14:16:32 +0000

    WHERE name LIKE BINARY {} ORDER BY states.id ASC

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -11,6 +11,7 @@ if __name__ == "__main__":
     """
     Connects to a MySQL database and retrieves data from the states table.
     """
+
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
     cursor = db.cursor()
