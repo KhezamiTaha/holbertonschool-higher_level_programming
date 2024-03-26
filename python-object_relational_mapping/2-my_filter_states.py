88c91f5182577ba3a5cdb82996c31bc35985a249
KhezamiTaha <khezamitaha10@gmail.com>
2024-03-26 14:20:03 +0000

    WHERE name LIKE BINARY {} ORDER BY states.id ASC

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -11,12 +11,11 @@ if __name__ == "__main__":
     """
     Connects to a MySQL database and retrieves data from the states table.
     """
-
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY states.id ASC"
-                   .format(sys.argv[4]))
+    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'\
+                    ORDER BY states.id ASC".format(sys.argv[4]))
     rows = cursor.fetchall()
     for row in rows:
         print(row)
