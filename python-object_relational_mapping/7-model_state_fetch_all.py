164cfacff6d67eca98795ffab9423ce0d4b6f6ed
KhezamiTaha <khezamitaha10@gmail.com>
2024-03-27 17:38:26 +0000

    fetvhing date using sqlalchemy

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Fetch data from table in database 
+""" Fetch data from table in database
 """
 import sys
 from model_state import Base, State
@@ -8,8 +8,9 @@ from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     first_session = Session()
-    
-
+    for objec_t in first_session.query(State).order_by(State.id.asc()).all():
+        print(f"{objec_t.id}: {objec_t.name}")
