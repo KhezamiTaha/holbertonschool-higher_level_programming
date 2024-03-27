b8bdc37fb2012c330231de0f34a6e0344eb9e263
KhezamiTaha <khezamitaha10@gmail.com>
2024-03-27 12:09:09 +0000

    trying sqlalchemy

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""create states class and base directive 
+"""
+
+import sqlalchemy
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
+class State(Base):
+    '''class will be mapped to a table
+    '''
+
+    __tablename__ = "states"
+
+    id = Column(Integer, nullable=False, unique=True, primary_key=True)
+    name = Column(String(128), nullable=False)
