f11e7de8fedc6f73897bddfec70f44cab6b2a1e3
KhezamiTaha <khezamitaha10@gmail.com>
2024-03-28 07:19:07 +0000

    insert and commit

---
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" Fetch data from table in database
+"""
+import sys
+
+from requests import session
+from model_state import Base, State
+
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    first_session = Session()
+    Lousiana_state = State(name="Louisiana")
+    first_session.add(Lousiana_state)
+    objec_ts = first_session.query(State.id).filter(
+            State.name == Lousiana_state.name).order_by(State.id.asc()).all()
+    if objec_ts:
+        for obj in objec_ts:
+            print(f"{obj[0]}")
+    else:
+        print("Not found")
+    first_session.commit()
