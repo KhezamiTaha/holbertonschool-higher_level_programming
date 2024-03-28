9910033be0ff42bbda48b1a47ab83539d4f20caf
KhezamiTaha <khezamitaha10@gmail.com>
2024-03-28 07:59:37 +0000

    Delete some states and commit

---
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Fetch data from table in database
+"""
+import sys
+from model_state import Base, State
+
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    first_session = Session()
+    for objec_t in first_session.query(State).filter(
+            State.name.like("%a%")).order_by(State.id.asc()).all():
+        first_session.delete(objec_t)
+    first_session.commit()
