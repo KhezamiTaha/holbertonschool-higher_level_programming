0e6d33dcf0b3285f2e65677f58e0df005059fd7c
KhezamiTaha <khezamitaha10@gmail.com>
2024-04-08 12:40:25 +0100

    debugging MySQL

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
     cursor = db.cursor()
-    query = "SELECT * FROM cities WHERE name = %s ORDER BY states.id"
+    query = "SELECT * FROM states WHERE name = %s ORDER BY states.id"
 
     cursor.execute(query, (sys.argv[4],))
     rows = cursor.fetchall()
