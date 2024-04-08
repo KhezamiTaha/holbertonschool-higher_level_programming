107a1c09f16e5b9b5768da13a0301cbd00f1d436
KhezamiTaha <khezamitaha10@gmail.com>
2024-04-08 12:23:30 +0100

    Python - Data Structures: Lists, Tuples

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     if not matrix:
-        return   #this will print None
+        return   # this will print None
     for roww in matrix:
         for i in range(len(roww)):
             print("{:d}".format(roww[i]), end="")
